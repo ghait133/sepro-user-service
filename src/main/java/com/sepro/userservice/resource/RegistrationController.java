df74889681568d50bd0886084efb731237ed6854
Ghait <ghait@mogic.com>
2019-07-26 17:56:20 +0200

    Adding Mail properties and activate the EventPublisher for the Registration

--- src/main/java/com/sepro/userservice/resource/RegistrationController.java
+++ src/main/java/com/sepro/userservice/resource/RegistrationController.java
@@ -4,12 +4,14 @@ import com.sepro.userservice.dto.UserDto;
 import com.sepro.userservice.entity.Role;
 import com.sepro.userservice.entity.User;
 import com.sepro.userservice.model.CustomPrincipal;
+import com.sepro.userservice.registration.OnRegistrationCompleteEvent;
 import com.sepro.userservice.repository.RoleRepository;
 import com.sepro.userservice.service.IUserService;
 import com.sepro.userservice.util.GenericResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -26,13 +28,16 @@ public class RegistrationController {
     @Autowired
     private RoleRepository roleRepository;
 
+    @Autowired
+    private ApplicationEventPublisher eventPublisher;
+
     @RequestMapping(value = "/registration", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public GenericResponse registerUserAccount(@Valid @RequestBody final UserDto accountDto, final HttpServletRequest request) {
         LOGGER.debug("Registering user account with information: {}", accountDto);
 
         final User registered = userService.registerNewUserAccount(accountDto);
-        //eventPublisher.publishEvent(new OnRegistrationCompleteEvent(registered, request.getLocale(), getAppUrl(request)));
+        eventPublisher.publishEvent(new OnRegistrationCompleteEvent(registered, request.getLocale(), getAppUrl(request)));
         return new GenericResponse("success");
     }
     @RequestMapping(
@@ -43,6 +48,10 @@ public class RegistrationController {
     public Role getUserByUserName (){
         Role user = roleRepository.findByName("role_admin");
         return user;
+    }
+
+    private String getAppUrl(HttpServletRequest request) {
+        return "http://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath();
     }
 
 }
